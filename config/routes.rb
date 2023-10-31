fdb11c6ba1191b7e09ddef0f7800fb41efd6c4ec
Frederico <frederico.smozzato@gmail.com>
2023-10-31 18:00:23 -0300

    Configura rotas para owners

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :owners, path: "owners"
+  devise_for :owners, path: "owners", controllers: {
+    sesions: "owners/sessions",
+    registrations: "owners/registrations",
+    passwords: "owners/passwords"
+  }
   devise_for :users
 
   root "home#index"
