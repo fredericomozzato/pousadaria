f130fd36feaa7c3e7c7ae79fc0b69ecb852f7974
Frederico <frederico.smozzato@gmail.com>
2023-10-31 18:23:03 -0300

    Refactor teste new_owner_account

--- spec/system/user/user_creates_owner_account_spec.rb
+++ spec/system/user/user_creates_owner_account_spec.rb
@@ -1,28 +1,30 @@
 require "rails_helper"
 
 describe "Usuário acessa a página de cadastro" do
-  it "a partir da página inicial" do
-    visit root_path
-    click_on "Entrar"
-    click_on "Proprietário"
-    click_on "Criar conta"
+  context "de proprietário" do
+    it "a partir da página inicial" do
+      visit root_path
+      click_on "Entrar"
+      click_on "Proprietário"
+      click_on "Criar conta"
 
-    expect(page).to have_field "E-mail"
-    expect(page).to have_field "Senha"
-    expect(page).to have_field "Confirmar senha"
-    expect(page).to have_button "Criar conta"
-  end
+      expect(page).to have_field "E-mail"
+      expect(page).to have_field "Senha"
+      expect(page).to have_field "Confirmar senha"
+      expect(page).to have_button "Criar conta"
+    end
 
-  it "e cria uma nova conta de Proprietário" do
-    visit new_owner_registration_path
-    fill_in "E-mail", with: "owner@example.com"
-    fill_in "Senha", with: "123456"
-    fill_in "Confirmar senha", with: "123456"
-    click_on "Criar conta"
+    it "e cria uma nova conta" do
+      visit new_owner_registration_path
+      fill_in "E-mail", with: "owner@example.com"
+      fill_in "Senha", with: "123456"
+      fill_in "Confirmar senha", with: "123456"
+      click_on "Criar conta"
 
-    within "nav" do
-      expect(page).to have_content "owner@example.com"
-      expect(page).to have_link "Sair"
+      within "nav" do
+        expect(page).to have_content "owner@example.com"
+        expect(page).to have_link "Sair"
+      end
     end
   end
 end
