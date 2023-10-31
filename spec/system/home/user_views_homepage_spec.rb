ba66cb87bc4afb9de3a7202e0a76df9c280f343e
Frederico <frederico.smozzato@gmail.com>
2023-10-31 17:15:46 -0300

    Implementa logout do modelo user

--- spec/system/home/user_views_homepage_spec.rb
+++ spec/system/home/user_views_homepage_spec.rb
@@ -1,13 +1,60 @@
 require "rails_helper"
 
 describe "Usuário visita a página inicial" do
-  it "e vê o menu" do
-    visit root_path
+  context "desautenticado" do
+    it "e vê o menu" do
+      visit root_path
 
-    within("nav") do
-      expect(page).to have_content "Pousadaria"
-      expect(page).to have_content "Entrar"
+      within "nav" do
+        expect(page).to have_content "Pousadaria"
+        expect(page).to have_link "Entrar"
+      end
     end
-    expect(page).to have_content "Nenhuma pousada cadastrada"
+
+    it "e faz login" do
+      User.create!(email: "user@example.com", password: "123456")
+
+      visit root_path
+      click_on "Entrar"
+      fill_in "E-mail", with: "user@example.com"
+      fill_in "Senha", with: "123456"
+      within "form" do
+        click_on "Entrar"
+      end
+
+      within "nav" do
+        expect(page).to have_link "Sair"
+        expect(page).to have_content "user@example.com"
+      end
+    end
+  end
+
+  context "autenticado" do
+    it "e vê o menu" do
+      user = User.create!(email: "user@example.com", password: "123456")
+
+      login_as user
+      visit root_path
+
+      within "nav" do
+        expect(page).to have_content "user@example.com"
+        expect(page).to have_link "Sair"
+        expect(page).not_to have_link "Entrar"
+      end
+    end
+
+    it "e faz logout" do
+      user = User.create!(email: "user@example.com", password: "123456")
+
+      login_as user
+      visit root_path
+      within "nav" do
+        click_on "Sair"
+      end
+
+      expect(page).to have_link "Entrar"
+      expect(page).not_to have_link "Sair"
+      expect(page).not_to have_content "user@example.com"
+    end
   end
 end
--- spec/system/user/user_creates_owner_account_spec.rb
+++ spec/system/user/user_creates_owner_account_spec.rb
@@ -11,4 +11,17 @@ describe "Usuário acessa a página de cadastro" do
     expect(page).to have_field "Confirmar senha"
     expect(page).to have_button "Criar conta"
   end
+
+  it "e cria uma nova conta de Dono de pousada" do
+    visit new_user_registration_path
+    fill_in "E-mail", with: "user@example.com"
+    fill_in "Senha", with: "123456"
+    fill_in "Confirmar senha", with: "123456"
+    click_on "Criar conta"
+
+    within "nav" do
+      expect(page).to have_content "user@example.com"
+      expect(page).to have_link "Sair"
+    end
+  end
 end
