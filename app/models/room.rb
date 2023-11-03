e18026949917bbb1f5aeab8ca10b30c48eeb970e
Frederico <frederico.smozzato@gmail.com>
2023-11-03 10:59:17 -0300

    Adiciona validação numérica em Room

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,6 +4,11 @@ class Room < ApplicationRecord
             :max_guests,
             :price,
             presence: true
+  validates :size,
+            :max_guests,
+            numericality: { greater_than: 0 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
 
   belongs_to :inn
 end
