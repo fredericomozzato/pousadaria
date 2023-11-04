6ffe8be128c42df3edc7ffba124f62677202bb48
Frederico <frederico.smozzato@gmail.com>
2023-11-04 10:01:52 -0300

    Restringe quartos para preços sazonais
    Garante que um proprietário só tenha acesso a quartos de sua pousada
    no formulário de preços sazonais

--- app/controllers/seasonal_prices_controller.rb
+++ app/controllers/seasonal_prices_controller.rb
@@ -1,5 +1,6 @@
 class SeasonalPricesController < ApplicationController
   def new
     @rooms = Room.where("inn_id = ? AND active = ?", current_owner.inn.id, true)
+    @seasonal_price = SeasonalPrice.new
   end
 end
