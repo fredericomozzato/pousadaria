d72e95928e3eec3023d4370db515d56d5298b0c4
Frederico <frederico.smozzato@gmail.com>
2023-11-06 09:38:40 -0300

    Adiciona testes de unidade para Preço Sazonal

--- app/models/seasonal_price.rb
+++ app/models/seasonal_price.rb
@@ -1,6 +1,8 @@
 class SeasonalPrice < ApplicationRecord
   belongs_to :room
-  validate :start_date_before_end_date, :end_date_is_past, :date_conflict
+  validates :start, :end, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validate :end_date_is_past, :date_conflict, :start_date_before_end_date, :start_equals_to_end
 
   def date_pretty_print
     formatter = "%d/%m/%Y"
@@ -10,18 +12,24 @@ class SeasonalPrice < ApplicationRecord
   private
 
   def start_date_before_end_date
-    if self.start >= self.end
+    return if self.start.nil? || self.end.nil?
+
+    if self.start > self.end
       errors.add :start, "Data de início não pode ser antes da data de término"
     end
   end
 
   def end_date_is_past
+    return if self.end.nil?
+
     if self.end < Date.today
       errors.add :end, "Data de término não pode estar no passado"
     end
   end
 
   def date_conflict
+    return if self.start.nil? || self.end.nil?
+
     room_seasonal_prices = SeasonalPrice.where("room_id = ?", self.room_id)
     room_seasonal_prices.each do |room|
       if (room.start..room.end).cover? self.start
@@ -29,6 +37,14 @@ class SeasonalPrice < ApplicationRecord
       elsif (room.start..room.end).cover? self.end
         errors.add :end, "Data de término conflita com outro preço sazonal"
       end
+    end
+  end
+
+  def start_equals_to_end
+    return if self.start.nil? || self.end.nil?
+
+    if self.start == self.end
+      errors.add :start, "Data de início não pode ser igual à data de término"
     end
   end
 end
---
+++ spec/models/seasonal_price_spec.rb
@@ -0,0 +1,157 @@
+require "rails_helper"
+
+RSpec.describe SeasonalPrice, type: :model do
+  describe "#valid?" do
+    it "inválido sem data de início" do
+      sp = SeasonalPrice.new(start: nil)
+
+      expect(sp.valid?).to be false
+      expect(sp.errors.include?(:start)).to be true
+      expect(sp.errors[:start]).to include("não pode ficar em branco")
+    end
+
+    it "inválido sem data de término" do
+      sp = SeasonalPrice.new(end: nil)
+
+      expect(sp.valid?).to be false
+      expect(sp.errors.include?(:end)).to be true
+      expect(sp.errors[:end]).to include("não pode ficar em branco")
+    end
+
+    it "inválido com data de início maior que data de término" do
+      sp = SeasonalPrice.new(start: Date.today, end: Date.yesterday)
+
+      expect(sp.valid?).to be false
+      expect(sp.errors.include?(:start)).to be true
+      expect(sp.errors[:start]).to include("Data de início não pode ser antes da data de término")
+    end
+
+    it "inválido com data de término no passado" do
+      sp = SeasonalPrice.new(end: Date.yesterday)
+
+      expect(sp.valid?).to be false
+      expect(sp.errors.include?(:end)).to be true
+      expect(sp.errors[:end]).to include("Data de término não pode estar no passado")
+    end
+
+    it "inválido com data de início igual data de término" do
+      sp = SeasonalPrice.new(start: 1.week.from_now, end: 1.week.from_now)
+
+      expect(sp.valid?).to be false
+      expect(sp.errors.include?(:start)).to be true
+      expect(sp.errors[:start]).to include("Data de início não pode ser igual à data de término")
+    end
+
+    it "inválido com data de início entre datas de outro preço sazonal" do
+      owner = Owner.create!(email: "owner@email.com", password: "123456")
+      inn = Inn.create!(name: "Pousada", corporate_name: "Pousada S/A",
+                    registration_number: "123445566799", phone: "999999999",
+                    email: "pousada@email.com", pay_methods: "Dinheiro",
+                    owner_id: owner.id)
+      room = Room.create!(name: "Quarto", size: 20, max_guests: 2, inn_id: inn.id,
+                          price: 200)
+      valid_sp = SeasonalPrice.create!(start: 1.week.from_now, end: 3.weeks.from_now, price: 300, room_id: room.id)
+      invalid_sp = SeasonalPrice.new(start: 2.weeks.from_now, end: 4.weeks.from_now, price: 400, room_id: room.id)
+
+      expect(invalid_sp.valid?).to be false
+      expect(invalid_sp.errors.include?(:start)).to be true
+      expect(invalid_sp.errors[:start]).to include("Data de início conflita com outro preço sazonal")
+    end
+
+    it "inválido com data de término entre datas de outro preço sazonal" do
+      owner = Owner.create!(email: "owner@email.com", password: "123456")
+      inn = Inn.create!(name: "Pousada", corporate_name: "Pousada S/A",
+                    registration_number: "123445566799", phone: "999999999",
+                    email: "pousada@email.com", pay_methods: "Dinheiro",
+                    owner_id: owner.id)
+      room = Room.create!(name: "Quarto", size: 20, max_guests: 2, inn_id: inn.id,
+                          price: 200)
+      valid_sp = SeasonalPrice.create!(start: 1.week.from_now, end: 3.weeks.from_now, price: 300, room_id: room.id)
+      invalid_sp = SeasonalPrice.new(start: 1.day.from_now, end: 2.weeks.from_now, price: 200, room_id: room.id)
+
+      expect(invalid_sp.valid?).to be false
+      expect(invalid_sp.errors.include?(:end)).to be true
+      expect(invalid_sp.errors[:end]).to include("Data de término conflita com outro preço sazonal")
+    end
+
+    it "inválido com data de início igual a data de in;icio de outro preço sazonal" do
+      owner = Owner.create!(email: "owner@email.com", password: "123456")
+      inn = Inn.create!(name: "Pousada", corporate_name: "Pousada S/A",
+                    registration_number: "123445566799", phone: "999999999",
+                    email: "pousada@email.com", pay_methods: "Dinheiro",
+                    owner_id: owner.id)
+      room = Room.create!(name: "Quarto", size: 20, max_guests: 2, inn_id: inn.id,
+                          price: 200)
+      valid_sp = SeasonalPrice.create!(start: 1.week.from_now, end: 3.weeks.from_now, price: 300, room_id: room.id)
+      invalid_sp = SeasonalPrice.new(start: 1.week.from_now, end: 4.weeks.from_now, price: 200, room_id: room.id)
+
+      expect(invalid_sp.valid?).to be false
+      expect(invalid_sp.errors.include?(:start)).to be true
+      expect(invalid_sp.errors[:start]).to include("Data de início conflita com outro preço sazonal")
+    end
+
+    it "inválido com data de início igual a data de término de outro preço sazonal" do
+      owner = Owner.create!(email: "owner@email.com", password: "123456")
+      inn = Inn.create!(name: "Pousada", corporate_name: "Pousada S/A",
+                    registration_number: "123445566799", phone: "999999999",
+                    email: "pousada@email.com", pay_methods: "Dinheiro",
+                    owner_id: owner.id)
+      room = Room.create!(name: "Quarto", size: 20, max_guests: 2, inn_id: inn.id,
+                          price: 200)
+      valid_sp = SeasonalPrice.create!(start: 1.week.from_now, end: 3.weeks.from_now, price: 300, room_id: room.id)
+      invalid_sp = SeasonalPrice.new(start: 3.weeks.from_now, end: 4.weeks.from_now, price: 200, room_id: room.id)
+
+      expect(invalid_sp.valid?).to be false
+      expect(invalid_sp.errors.include?(:start)).to be true
+      expect(invalid_sp.errors[:start]).to include("Data de início conflita com outro preço sazonal")
+    end
+
+    it "inválido com data de término igual a data de início de outro preço sazonal" do
+      owner = Owner.create!(email: "owner@email.com", password: "123456")
+      inn = Inn.create!(name: "Pousada", corporate_name: "Pousada S/A",
+                    registration_number: "123445566799", phone: "999999999",
+                    email: "pousada@email.com", pay_methods: "Dinheiro",
+                    owner_id: owner.id)
+      room = Room.create!(name: "Quarto", size: 20, max_guests: 2, inn_id: inn.id,
+                          price: 200)
+      valid_sp = SeasonalPrice.create!(start: 1.week.from_now, end: 3.weeks.from_now, price: 300, room_id: room.id)
+      invalid_sp = SeasonalPrice.new(start: Date.today, end: 1.week.from_now, price: 100, room_id: room.id)
+
+      expect(invalid_sp.valid?).to be false
+      expect(invalid_sp.errors.include?(:end)).to be true
+      expect(invalid_sp.errors[:end]).to include("Data de término conflita com outro preço sazonal")
+    end
+
+    it "inválido com data de término igual a data de término de outro preço sazonal" do
+      owner = Owner.create!(email: "owner@email.com", password: "123456")
+      inn = Inn.create!(name: "Pousada", corporate_name: "Pousada S/A",
+                    registration_number: "123445566799", phone: "999999999",
+                    email: "pousada@email.com", pay_methods: "Dinheiro",
+                    owner_id: owner.id)
+      room = Room.create!(name: "Quarto", size: 20, max_guests: 2, inn_id: inn.id,
+                          price: 200)
+      valid_sp = SeasonalPrice.create!(start: 1.week.from_now, end: 3.weeks.from_now, price: 300, room_id: room.id)
+      invalid_sp = SeasonalPrice.new(start: Date.today, end: 3.weeks.from_now, price: 100, room_id: room.id)
+
+      expect(invalid_sp.valid?).to be false
+      expect(invalid_sp.errors.include?(:end)).to be true
+      expect(invalid_sp.errors[:end]).to include("Data de término conflita com outro preço sazonal")
+    end
+
+    it "inválido sem preço" do
+      sp = SeasonalPrice.new(price: nil)
+
+      expect(sp.valid?).to be false
+      expect(sp.errors.include?(:price)).to be true
+      expect(sp.errors[:price]).to include("não pode ficar em branco")
+    end
+
+    it "inválido com preço menor ou igual a 0" do
+      sp = SeasonalPrice.new(price: 0)
+
+      expect(sp.valid?).to be false
+      expect(sp.errors.include?(:price)).to be true
+      expect(sp.errors[:price]).to include("deve ser maior que 0")
+    end
+  end
+end
