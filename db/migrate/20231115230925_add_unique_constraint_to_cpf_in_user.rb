daf36376744ccf7d5e4991b0e639f3c7f9be54b2
Frederico <frederico.smozzato@gmail.com>
2023-11-15 20:10:58 -0300

    Adiciona constraint unique CPF no model e schema

---
+++ db/migrate/20231115230925_add_unique_constraint_to_cpf_in_user.rb
@@ -0,0 +1,5 @@
+class AddUniqueConstraintToCpfInUser < ActiveRecord::Migration[7.0]
+  def change
+    add_index :users, :cpf, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_15_205657) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_15_230925) do
   create_table "addresses", force: :cascade do |t|
     t.string "street"
     t.string "number"
@@ -115,6 +115,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_15_205657) do
     t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
     t.string "cpf", default: "", null: false
+    t.index ["cpf"], name: "index_users_on_cpf", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
