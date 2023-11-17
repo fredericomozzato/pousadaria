7b606dc908ce9b63b4493b1d757a83ac9ab27a5e
Frederico <frederico.smozzato@gmail.com>
2023-11-17 16:33:45 -0300

    Incrementa testes de sistema para reservas

--- spec/system/bookings/owner_views_bookings_spec.rb
+++ spec/system/bookings/owner_views_bookings_spec.rb
@@ -9,7 +9,7 @@ describe "Proprietário acessa a página de reservas da pousada" do
     expect(current_path).to eq new_owner_session_path
   end
 
-  it "quando não tem reservas confirmadas" do
+  it "e não tem reservas confirmadas" do
     owner = Owner.create!(
           email: "owner@email.com",
           password: "123456"
@@ -91,18 +91,40 @@ describe "Proprietário acessa a página de reservas da pousada" do
       price: 200.00,
       inn_id: inn.id
     )
-    user = User.create!(
-        name: "User",
+    room_mar = Room.create!(
+      name: "Mar",
+      description: "Quarto com vista para o mar",
+      size: 40,
+      max_guests: 3,
+      price: 300.00,
+      inn_id: inn.id
+    )
+    user_1 = User.create!(
+        name: "User 1",
         cpf: "820.628.780-95",
-        email: "user@email.com",
+        email: "user1@email.com",
         password: "123456"
       )
-    booking = Booking.create!(
+    user_2 = User.create!(
+      name: "User 2",
+        cpf: "820.628.780-96",
+        email: "user2@email.com",
+        password: "654321"
+    )
+    booking_1 = Booking.create!(
       room: room_ocean,
-      user: user,
+      user: user_1,
       start_date: 1.week.from_now,
       end_date: 2.weeks.from_now,
-      number_of_guests: 2,
+      number_of_guests: 2
+    )
+    booking_2 = Booking.create!(
+      room: room_mar,
+      user: user_2,
+      start_date: Date.today,
+      end_date: 3.days.from_now,
+      number_of_guests: 3,
+      status: :active
     )
 
     login_as owner
@@ -111,11 +133,18 @@ describe "Proprietário acessa a página de reservas da pousada" do
     expect(page).not_to have_content "Nenhuma reserva encontrada"
     within "section#bookings-list" do
       expect(page).to have_content "Reserva:"
-      expect(page).to have_link booking.code
+      expect(page).to have_link booking_1.code
       expect(page).to have_content "Quarto: Oceano"
-      expect(page).to have_content "Data de check-in: #{I18n.l(booking.start_date)}"
-      expect(page).to have_content "Data de check-out: #{I18n.l(booking.end_date)}"
-      expect(page).to have_content "Número de hóspedes: #{booking.number_of_guests}"
+      expect(page).to have_content "Data de check-in: #{I18n.l(booking_1.start_date)}"
+      expect(page).to have_content "Data de check-out: #{I18n.l(booking_1.end_date)}"
+      expect(page).to have_content "Número de hóspedes: #{booking_1.number_of_guests}"
+      expect(page).to have_content "Status: Confirmada"
+      expect(page).to have_link booking_2.code
+      expect(page).to have_content "Quarto: Mar"
+      expect(page).to have_content "Data de check-in: #{I18n.l(booking_2.start_date)}"
+      expect(page).to have_content "Data de check-out: #{I18n.l(booking_2.end_date)}"
+      expect(page).to have_content "Número de hóspedes: #{booking_2.number_of_guests}"
+      expect(page).to have_content "Status: Ativa"
     end
   end
 
