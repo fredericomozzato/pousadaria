9ad7da9bba530e270b790ee50e05abcf41529cfc
Frederico <frederico.smozzato@gmail.com>
2023-11-22 13:57:07 -0300

    Refactor das views que apresentam a avaliação

--- spec/system/reviews/user_reviews_inn_spec.rb
+++ spec/system/reviews/user_reviews_inn_spec.rb
@@ -401,4 +401,69 @@ describe "Usuário acessa a página de avaliação" do
     expect(current_path).to eq root_path
     expect(page).to have_content "Não foi possível completar a requisição"
   end
+
+  it "e vê resposta do proprietário em sua avaliação" do
+    owner = Owner.create!(email: "dono_1@email.com", password: "123456")
+    inn = Inn.create!(
+      name: "Mar Aberto",
+      corporate_name: "Pousada Mar Aberto/SC",
+      registration_number: "84.485.218/0001-73",
+      phone: "4899999-9999",
+      email: "pousadamaraberto@hotmail.com",
+      description: "Pousada na beira do mar com suítes e café da manhã incluso.",
+      pay_methods: "Crédito, débito, dinheiro ou pix",
+      pet_friendly: true,
+      user_policies: "A pousada conta com lei do silêncio das 22h às 8h",
+      check_in_time: Time.new(2000, 1, 1, 9, 0, 0, "UTC"),
+      check_out_time: Time.new(2000, 1, 1, 15, 30, 0, "UTC"),
+      owner: owner
+    )
+    Address.create!(
+      street: "Rua das Flores",
+      number: 300,
+      neighborhood: "Canasvieiras",
+      city: "Florianópolis",
+      state: "SC",
+      postal_code: "88000-000",
+      inn: inn
+    )
+    room = Room.create!(
+      name: "Atlântico",
+      description: "Quarto com vista para o mar",
+      size: 30,
+      max_guests: 2,
+      price: 200.00,
+      inn: inn,
+      bathroom: true,
+      wifi: true,
+      wardrobe: true,
+      accessibility: true
+    )
+    user = User.create!(
+      name: "João Silva",
+      cpf: "899.924.320-63",
+      email: "joao@email.com",
+      password: "123456"
+    )
+    booking = Booking.create!(
+      room: room,
+      user: user,
+      start_date: Date.today,
+      end_date: 5.days.from_now,
+      number_of_guests: 2,
+      status: :closed
+    )
+    review = Review.create!(
+      score: 5,
+      message: "Ótima pousada",
+      answer: "Obrigado!",
+      booking: booking
+    )
+
+    login_as user, scope: :user
+    visit my_bookings_path
+    click_on booking.code
+
+    expect(page).to have_content "Resposta: Obrigado!"
+  end
 end
