d7f972189e7a9589aaea857169daeba1f8bbbce4
Frederico <frederico.smozzato@gmail.com>
2023-11-23 16:45:43 -0300

    Traduz mensagens de erro no model Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -68,15 +68,15 @@ class Booking < ApplicationRecord
   def past_dates
     return if start_date.nil? || end_date.nil?
 
-    errors.add(:start_date, "não pode ser no passado") if start_date < Date.today
-    errors.add(:end_date, "não pode ser no passado") if end_date < Date.today
-    errors.add(:end_date, "não pode ser antes da Data de Check-in") if end_date < start_date
+    errors.add(:start_date, I18n.t("cant_be_past")) if start_date < Date.today
+    errors.add(:end_date, I18n.t("cant_be_past")) if end_date < Date.today
+    errors.add(:end_date, I18n.t("cant_be_before_checkin")) if end_date < start_date
   end
 
   def active_room
     room = Room.find_by(id: room_id)
 
-    errors.add(:room, "indisponível") unless room&.active?
+    errors.add(:room, I18n.t("room_unavailable")) unless room&.active?
   end
 
   def generate_code
